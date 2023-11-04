1c9b6ff5493ba503bebf9fe88f80e3576f47963e
tataadin <tataa2237@gmail.com>
2023-11-04 20:14:58 +0700

    [feature][widgets] add new widget user_circle_image

---
+++ lib/widgets/user_circle_image.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class UserCircleImage extends StatelessWidget {
+  const UserCircleImage(
+      {Key? key, required this.imgUrl, required this.diameter})
+      : super(key: key);
+
+  final String imgUrl;
+  final double diameter;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: diameter,
+      height: diameter,
+      decoration: BoxDecoration(
+          border: Border.all(width: 2, color: Colors.grey),
+          shape: BoxShape.circle,
+          image:
+              DecorationImage(fit: BoxFit.cover, image: NetworkImage(imgUrl))),
+    );
+  }
+}
