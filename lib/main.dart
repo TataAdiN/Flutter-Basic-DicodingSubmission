608ee25d541f84dac2cd92f794473a11a915c963
tataadin <tataa2237@gmail.com>
2023-11-04 20:12:03 +0700

    [refactor][main] change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Monumen Bersejarah - Dicoding Submission',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Monumen Bersejarah'),
     );
   }
 }
