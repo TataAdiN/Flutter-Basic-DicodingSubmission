9de5081dff462514bb835f75421643cc2e6895a7
tataadin <tataa2237@gmail.com>
2023-11-05 18:07:48 +0700

    [refactor][model.monument] rename mapLink to pinMap

--- lib/models/monument.dart
+++ lib/models/monument.dart
@@ -1,12 +1,12 @@
 class Monument {
-  String name, summary, map, mapLink, description;
+  String name, summary, map, pinMap, description;
   List<String> images;
 
   Monument(
       {required this.name,
       required this.summary,
       required this.map,
-      required this.mapLink,
+      required this.pinMap,
       required this.description,
       required this.images});
 }
