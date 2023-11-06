70b8a4cc66f4bdf4b7f6f3a09a4b9348ee8e263a
tataadin <tataa2237@gmail.com>
2023-11-06 19:20:33 +0700

    [refactor][page.monument|page.profile] make statelesswidget

--- lib/pages/monumen/monumen_page.dart
+++ lib/pages/monumen/monumen_page.dart
@@ -1,13 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../models/monument.dart';
-import 'monumen_state.dart';
 
-class MonumentPage extends StatefulWidget {
+class MonumentPage extends StatelessWidget {
   const MonumentPage({super.key, required this.monument});
 
   final Monument monument;
 
+  void _openMaps(String pinMap) {
+    canLaunchUrl(Uri.parse(pinMap))
+        .then((value) =>
+        launchUrl(Uri.parse(pinMap), mode: LaunchMode.externalApplication))
+        .onError((error, stackTrace) {
+      return false;
+    });
+  }
+
   @override
-  State<MonumentPage> createState() => MonumentPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(monument.name),
+        ),
+        body: Container(
+          color: Colors.grey[300],
+          child: ListView(
+            children: [
+              const SizedBox(height: 10.0),
+              imagesCard(context, monument.images),
+              detailCard(context)
+            ],
+          ),
+        ));
+  }
+
+  Widget imagesCard(BuildContext context, List<String> images) => Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        FlutterCarousel(
+          options: CarouselOptions(
+            height: MediaQuery.of(context).size.width * (60 / 100),
+            showIndicator: true,
+            floatingIndicator: false,
+            slideIndicator: CircularSlideIndicator(
+                currentIndicatorColor:
+                Theme.of(context).colorScheme.inversePrimary,
+                indicatorBackgroundColor: Colors.grey[200]),
+          ),
+          items: images.map((String image) {
+            return Container(
+              height: MediaQuery.of(context).size.width * (50 / 100),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: Colors.grey),
+                  shape: BoxShape.rectangle,
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/images/$image'))),
+            );
+          }).toList(),
+        )
+      ]));
+
+  Widget detailCard(BuildContext context) => Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      child: Card(
+          color: Colors.white,
+          elevation: 0,
+          child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Sejarah Monument',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(monument.description,
+                        textAlign: TextAlign.justify),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Dapatkan Peta Lokasi',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    mapButton(context, () => _openMaps(monument.pinMap))
+                  ]))));
+
+  Widget mapButton(BuildContext context, VoidCallback onTap) => SizedBox(
+    width: MediaQuery.of(context).size.width * (40 / 100),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.grey[400],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          )),
+      onPressed: () => onTap(),
+      child: const Row(
+        children: [
+          Icon(Icons.pin_drop_outlined, color: Colors.white),
+          SizedBox(width: 5),
+          Text('Google Maps'),
+        ],
+      ),
+    ),
+  );
 }
--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,10 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-import 'profile_state.dart';
+import '../../widgets/user_circle_image.dart';
 
-class ProfilePage extends StatefulWidget {
+class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
+  void _openUrl(String url) async {
+    canLaunchUrl(Uri.parse(url))
+        .then((value) =>
+            launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication))
+        .onError((error, stackTrace) {
+      return false;
+    });
+  }
+
   @override
-  State<ProfilePage> createState() => ProfilePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Profil'),
+        ),
+        body: Container(
+          color: Colors.grey[300],
+          child: ListView(
+            children: [
+              const SizedBox(height: 10.0),
+              aboutCard(),
+              mediaSocialCard()
+            ],
+          ),
+        ));
+  }
+
+  Widget aboutCard() => const Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        child: Card(
+            color: Colors.white,
+            elevation: 0,
+            child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Tentang Aplikasi',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                          'Tema Aplikasi ini adalah Monumen Bersejarah yang ada di Daerah Istimewa Yogyakarta. Monumen yang menyimpanberbagai cerita sejarah perjuangan mulai dari sebelum kemerdekaan hingga pasca kemerdekaan Indonesia'
+                          '\n\nWarna merah dipilih untuk warna aplikasi terinspirasi logo Pariwisata Daerah Istimewa Yogyakarta',
+                          textAlign: TextAlign.justify)
+                    ]))),
+      );
+
+  Widget mediaSocialCard() => Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      child: Card(
+          color: Colors.white,
+          elevation: 0,
+          child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  const UserCircleImage(
+                      imgUrl:
+                          'https://scontent.fjog1-1.fna.fbcdn.net/v/t39.30808-6/395488480_2399833326890796_3346072083590875990_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFd8JT_if5ylcsa_hdf5xtGpqpXDqthw1amqlcOq2HDViyzbtlejMNsyCt7e_9rB41-px9b5HaI-N7MUOWn4QPY&_nc_ohc=ibYKLR_yaB0AX9Jc0Vl&_nc_ht=scontent.fjog1-1.fna&oh=00_AfD3LXnZJDu22kEUusPxSCbNEilZqRfprXY8d-IT-FB0_Q&oe=654B7E78',
+                      diameter: 120),
+                  const Center(
+                    child: Text(
+                      'Tata Adi Nugroho',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const Center(
+                    child: Text(
+                      'tataa2237@gmail.com',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  socialButton(() async {
+                    return _openUrl('https://github.com/TataAdiN');
+                  }, Colors.grey, LineIcons.github, 'Github'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  socialButton(() async {
+                    return _openUrl('https://www.facebook.com/Tata.AdiNugroho');
+                  }, Colors.lightBlue, LineIcons.facebook, 'Facebook'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  socialButton(() async {
+                    return _openUrl('https://www.linkedin.com/in/tataadin/');
+                  }, Colors.blue, LineIcons.linkedin, 'Linkedin'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  socialButton(() async {
+                    return _openUrl('https://www.instagram.com/tataadin.id/');
+                  }, Colors.redAccent, LineIcons.instagram, 'Instagram'),
+                  const SizedBox(
+                    height: 10,
+                  )
+                ],
+              ))));
+
+  Widget socialButton(VoidCallback onTap, Color backgroundColor, IconData icon,
+          String label) =>
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: backgroundColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            )),
+        onPressed: () => onTap(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(icon, color: Colors.white),
+            const SizedBox(width: 5),
+            Text(label),
+          ],
+        ),
+      );
 }
